4ad0e5bcb1c34b37f94e98a322ec849834342613
Tranel Bland <tranelb@gmail.com>
2016-11-30 14:18:56 -0800

    initial commit

--- Program2/Program.cs
+++ Program2/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Program2
+namespace Program1
 {
-    class Program
+    public class Program
     {
+        //This is wehre your program starts.
         static void Main(string[] args)
         {
+            //Prompt user to enter a name.
+            Console.WriteLine("Ener your name, please:");
+
+            //Now read the name entered.
+            string name = Console.ReadLine();
+
+            //Greet the user with the name that was entered.
+            Console.WriteLine("Hello, " + name);
+
+            //Wait for user to acknowledge the results.
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+            Console.WriteLine();
         }
     }
 }
